878e420392895332ac9beccad6e36db9c94819e1
小汤河职业技术学院 <805237796@qq.com>
2018-01-08 22:50:18 +0800

    26

--- 26.Remove Duplicates from Sorted Array/main.c
+++ 26.Remove Duplicates from Sorted Array/main.c
@@ -1,5 +1,17 @@
-#include <stdio.h>
+/*26. Remove Duplicates from Sorted Array
+Given a sorted array, remove the duplicates in-place such that each element appear only once and return the new length.
 
+Do not allocate extra space for another array, you must do this by modifying the input array in-place with O(1) extra memory.
+
+Example:
+
+Given nums = [1,1,2],
+
+Your function should return length = 2, with the first two elements of nums being 1 and 2 respectively.
+It doesn't matter what you leave beyond the new length.
+
+去除重复元素*/
+#include <stdio.h>
 int removeDuplicates(int* nums, int numsSize)//i,j前后错开，前后相同时，i 后移，找到 i,j 不同时，把当前 j+1 位置赋值成 i, i,j 同时后移
 {
     if(numsSize <= 1)
