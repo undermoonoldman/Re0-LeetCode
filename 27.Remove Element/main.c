89bdb5b36bcc9e7e44fdf97bff199aab1889c529
小汤河职业技术学院 <805237796@qq.com>
2018-01-08 22:45:48 +0800

    27

---
+++ 27.Remove Element/main.c
@@ -0,0 +1,117 @@
+#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+
+void swap(int* a,int* b)
+{
+    int temp = *a;
+    *a = *b;
+    *b = temp;
+}
+
+int __partition(int arr[], int l, int r)
+{
+    swap(&arr[l], &arr[rand() % (r-l+1) + l]);
+    int i = l+1;
+    int j = r;
+
+    while(1)
+    {
+        while(arr[i] < arr[l] && i <= r) i++;
+        while(arr[j] > arr[l] && j >= l+1) j--;
+
+        if(i >= j) break;
+
+        swap(&arr[i], &arr[j]);
+
+        i++;
+        j--;
+    }
+    swap(&arr[l], &arr[j]);
+    return j;
+}
+
+
+void __quickSort(int arr[], int l, int r)
+{
+    if(l >= r)
+
+        return;
+    int p = __partition(arr, l, r);
+    __quickSort(arr, l, p-1);
+    __quickSort(arr, p+1, r);
+}
+
+
+void quickSort(int arr[], int n)
+{
+
+    srand(time(NULL));
+
+    __quickSort(arr, 0, n-1);
+}
+
+// int removeElement(int* nums, int numsSize, int val)//方法1，先排序，再把大于 val 的部分覆盖到等于 val 的部分
+// {
+//    quickSort(nums, numsSize);
+//    int i = 0;
+//    int j;
+//    int ret = numsSize;
+//    while(i < numsSize)
+//    {
+//        if(nums[i] != val)
+//            i++;
+//        else
+//            break;
+//    }
+
+//    if(i >= numsSize)
+//        return numsSize;
+//    j = i;
+
+//    while(i < numsSize)
+//    {
+//        if(nums[i] == val)
+//        {
+//            i++;
+//            ret--;
+//        }
+//        else
+//            break;
+//    }
+
+//    if(i >= numsSize)
+//        return ret;
+
+//    for(int k = i ; k < numsSize ; k++)
+//        nums[j++] = nums[k];
+
+//    return ret;
+// }
+
+int removeElement(int* nums, int numsSize, int val)//方法2，把不等于 val 的部分移到数组的前面，后面全等于 val的部分
+{
+    int pos = 0;
+    for(int i = 0 ; i < numsSize ; i++)
+    {
+    	if(nums[i] != val)
+    		nums[pos++] = nums[i];
+    }
+    return pos;
+}
+
+void showIntArray(int *array, int n)
+{
+    for(int i = 0 ; i < n ; i++)
+        printf("%d ",array[i]);
+    printf("\n");
+}
+
+int main() {
+    printf("Hello, World!\n");
+    int a[] = {3,2,2,3};
+    showIntArray(a,4);
+    int len = removeElement(a, 4, 3);
+    showIntArray(a,len);
+    return 0;
+}
